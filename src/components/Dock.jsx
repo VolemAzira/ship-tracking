3b1284e037b915f17a638604c2291e61f3844f19
VolemAzira <volemalvaroazira2@gmail.com>
2023-10-01 18:34:51 +0700

    port pointer

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { Marker, Popup } from "react-leaflet";
+import { Icon } from "leaflet";
+import Anchors from "../assets/anchors.png";
 
 const Dock = ({ position, title, capacity }) => {
+  const customIcon = new Icon({
+    iconUrl: Anchors,
+    iconSize: [50, 50],
+  });
   return (
-    <Marker position={position} title={title}>
+    <Marker position={position} title={title} icon={customIcon}>
       <Popup>
         {title}
         <p>Capacity: {capacity} Ships</p>
